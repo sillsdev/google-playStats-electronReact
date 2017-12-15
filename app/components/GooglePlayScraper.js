826eb02ba4f94e57be6b843f17bc1e61f8c02035
Richard MacLean <rick_maclean@sil.org>
2017-12-15 14:01:24 -0600

    display one item from google play scraper search

--- app/components/GooglePlayScraper.js
+++ app/components/GooglePlayScraper.js
@@ -12,6 +12,7 @@ class GooglePlayScraper extends Component {
   props: {
   };
   state: {
+    packageNameFromCsv: string,
     googlePlaySearchStr: string,
     scraperSearchResults: array,
     scraperAppResults: array
@@ -19,7 +20,9 @@ class GooglePlayScraper extends Component {
   constructor() {
     super();
     this.getGooglePlaySearchResults = this.getGooglePlaySearchResults.bind(this);
+    this.getGooglePlayAppResults = this.getGooglePlayAppResults.bind(this);
     this.state = {
+      packageNameFromCsv: 'packageNameFromCsv',
       googlePlaySearchStr: 'some googlePlaySearchStr',
       scraperSearchResults: ['a'],
       scraperAppResults: ['a']
@@ -85,10 +88,19 @@ class GooglePlayScraper extends Component {
       num: 1
     }).then((value) => {
       this.setState({ scraperSearchResults: value });
+      let packageNm = value[0].appId;
+      this.setState({ packageNameFromCsv: packageNm });
+      console.log('value ===>');
       console.log(value);
+      console.log('value[0] ===>');
+      console.log(value[0]);
+      console.log('value[0].appId ===>');
+      console.log(value[0].appId);
     });
     console.log('here are the results =============>');
     console.log(results2);
+
+
 
     console.log('leaving getGooglePlaySearchResults');
   }
@@ -108,7 +120,7 @@ class GooglePlayScraper extends Component {
     console.log('leaving gsutilRunACommand');
   }
 
-  handleTextLocalState = (event) => {
+  handleSearchStringChange = (event) => {
     const target = event.target;
     console.log(target.name);
     console.log(target.value);
@@ -119,22 +131,33 @@ class GooglePlayScraper extends Component {
   render() {
     //  const { emailAddy } = this.props;
     //  const { propInHome } = this.props;
+    let packageNameFromCsv = 'packageNameFromCsv';
+    packageNameFromCsv = this.state.packageNameFromCsv;
     return (
       <div className="panel panel-primary">
         <div className="panel-heading apt-addheading">Select CSV Google Play Store Stats File</div>
         <div className="panel-body">
           <form className="form" onSubmit={this.localHandleSend}>
-            <div className="form-group">
-              <div className="col-sm-offset-3 col-sm-9">
-                <div className="pull-left">
-                  <button
-                    type="button"
-                    className="btn btn-primary"
-                    onClick={this.getGooglePlaySearchResults}
-                  >get GooglePlay SearchResults</button>&nbsp;
-                </div>
-              </div>  {/* className="col-sm-offset-3 col-sm-9" */}
-            </div> {/* form-group */}
+            <div className="container">
+              <div className="btn-toolbar" role="group" aria-label="Basic example">
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={this.getGooglePlaySearchResults}
+                >get GooglePlay SearchResults</button>
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={this.getGooglePlayAppResults}
+                >get GooglePlay App Full Results</button>
+              </div>
+              <br></br><br></br>
+              <div className="form-group">
+                <label className="col-sm-3 control-label" htmlFor="packageNameFromCsv">Package Name</label>
+                <div className="form-text" id="packageNameFromCsv" placeholder="packageNameFromCsv" >{packageNameFromCsv}</div>
+                <br></br>
+              </div> {/* form-group */}
+            </div>
             <div className="form-group">
               <label className="col-sm-3 control-label" htmlFor="googlePlaySearchStr">Google Play App SearchString</label>
               <div className="col-sm-9">
@@ -142,26 +165,27 @@ class GooglePlayScraper extends Component {
                   name="googlePlayAppSearchStringBox"
                   type="text" className="form-control"
                   id="googlePlayAppSearchString"
-                  onChange={this.handleTextLocalState}
+                  onChange={this.handleSearchStringChange}
                   value={this.state.googlePlayAppSearchString}
                   placeholder="googlePlayAppSearchString"
                 />
               </div>
             </div> {/* form-group */}
             <br></br><br></br>
-            <div className="form-group">
-              <div className="col-sm-offset-3 col-sm-9">
-                <div className="pull-right">
-                  <button
-                    type="button"
-                    className="btn btn-primary"
-                    onClick={this.gsutilRunACommand}
-                  >run gsutil download stat files for </button>&nbsp;
-                </div>
-                <label className="col-sm-offset-6 col-sm-6 control-label" htmlFor="gsutilProjectName">scriptureearth.acrn.nt.apk</label>
-              </div>  {/* className="col-sm-offset-3 col-sm-9" */}
-
-            </div> {/* form-group */}
+            <div className="container">
+              <div className="form-group">
+                <div className="col-sm-offset-1 col-sm-9">
+                  <div className="pull-left">
+                    <button
+                      type="button"
+                      className="btn btn-primary"
+                      onClick={this.gsutilRunACommand}
+                    >run gsutil download stat files for </button>&nbsp;
+                  </div>
+                  <label className="col-sm-6 control-label" htmlFor="gsutilProjectName">scriptureearth.acrn.nt.apk</label>
+                </div>  {/* className="col-sm-offset-3 col-sm-9" */}
+              </div> {/* form-group */}
+            </div>
           </form>
         </div>
       </div>
